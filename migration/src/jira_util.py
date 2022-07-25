7f64d3aa250636558bfa717a5b6edfdd5692806a
Michael McCandless <mikemccand@apache.org>
2022-07-25 09:58:49 -0400

    61: map Jira priority to legacy-jira-priority, and include votes in the 'Legacy Jira Information' header when it's > 0 (#76)

--- migration/src/jira_util.py
+++ migration/src/jira_util.py
@@ -53,6 +53,22 @@ def extract_resolution(o: dict) -> Optional[str]:
     return None
 
 
+def extract_priority(o: dict) -> Optional[str]:
+    priority = o.get("fields").get("priority")
+    if priority:
+        return priority.get("name")
+    return None
+
+
+def extract_vote_count(o: dict) -> Optional[str]:
+    votes = o.get("fields").get("votes")
+    if votes:
+        vote_count = votes.get("votes")
+        if vote_count > 0:
+            return vote_count
+    return None
+
+
 def extract_reporter(o: dict) -> tuple[str, str]:
     reporter = o.get("fields").get("reporter")
     name = reporter.get("name", "") if reporter else ""
